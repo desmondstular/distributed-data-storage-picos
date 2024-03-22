e1e24c1c857c5eef91dad60bb685819167430896
Desmond Stular <dstular@yahoo.com>
2024-03-22 12:27:40 -0600

    Added receiver fsm and reformatted code

--- app.cc
+++ app.cc
@@ -5,7 +5,7 @@
  * Authors: Ben Nourse
  *          Desmond Stular (3067040)
  *    Date: March 11, 2024
-*/
+ */
 
 #include "app.h"
 
@@ -17,67 +17,79 @@ byte groupID = 1;
 byte nodeID = 1;
 
 // Database and current size
+struct dbEntry db[DATABASE_SIZE];
 byte entryCount = 0;
 
-
 /*
- * Root fsm that acts as the user interface.
+ * Receiver fsm: Waits for packets from other
+ * nodes and then processes them.
 */
-fsm root {
-	int c;
+fsm receiver
+{
 	address packet;
-	struct discoveryMsg msg;
-	
-	// Initialize node
-	state INIT_SESSION:
-		phys_cc1350 (0, PACKET_LEN);
-		
-		tcv_plug(0, &plug_null);
-		sfd = tcv_open(NONE, 0, 0);
-		
-		// If session did not open
-		if (sfd < 0) {
-			diag("unable to open TCV session");
-			syserror(EASSERT, "no session");
-		}
-		
-		tcv_control(sfd, PHYSOPT_ON, NULL);
-		
-	// Show sender menu and get input
-	state MENU:
-		// show menu
-		ser_outf(MENU, "\r\nGroup %u Device #%u (%u/%u records)\r\n(G)roup ID\r\n(N)ew device ID\n\r(F)ind neighbors\r\n(C)reate record on neighbor\r\n(D)elete record on neighbor\r\n(R)etrieve record from neighbor\r\n(S)how local records\r\nR(e)set local storage\r\n\r\nSelection: ", groupID, nodeID, entryCount, DATABASE_SIZE);
-		
-		// Get user input
-		ser_inf(MENU, "%c", &c);
-		
-		c = toUppercase(c);
-	
-		
-		switch (c) {
-			case 'G':
-				proceed MENU;	//todo
-			case 'N':
-				proceed MENU;	//todo
-			case 'F':
-				proceed MENU;	//todo
-			case 'C':
-				proceed MENU;	//todo
-			case 'D':
-				proceed MENU;	//todo
-			case 'R':
-				proceed MENU;	//todo
-			case 'S':
-				proceed MENU;	//todo
-			case 'E':
-				proceed MENU;	//todo
-			default:
-				proceed MENU;	//todo
+
+	// Wait to receive packet
+	state WAIT_PKT:
+		packet = tcv_rnp(WAIT_PKT, sfd);
+
+	// Received packet
+	state RECEIVED_PKT:
+		byte* type = (byte*)(packet+2);
+
+		// Check type of packet
+		switch (*type) {
+			case DIS_REQ: 	proceed WAIT_PKT;	//todo
+			case DIS_RES: 	proceed WAIT_PKT;	//todo
+			case NEW_REC: 	proceed WAIT_PKT;	//todo
+			case DEL_REC: 	proceed WAIT_PKT;	//todo
+			case RET_REC: 	proceed WAIT_PKT;	//todo
+			case RES_MSG: 	proceed WAIT_PKT;	//todo
+			default: 		proceed WAIT_PKT;
 		}
 }
 
+/*
+ * Root fsm that acts as the user interface.
+ */
+fsm root
+{
+	int c;
+	address packet;
+	struct discoveryMsg msg;
 
+	// Initialize node
+	state INIT_SESSION : phys_cc1350(0, PACKET_LEN);
 
+	tcv_plug(0, &plug_null);
+	sfd = tcv_open(NONE, 0, 0);
 
+	// If session did not open
+	if (sfd < 0)
+	{
+		diag("unable to open TCV session");
+		syserror(EASSERT, "no session");
+	}
 
+	tcv_control(sfd, PHYSOPT_ON, NULL);
 
+	// Show sender menu and get input
+	state MENU :
+		ser_outf(MENU, "\r\nGroup %u Device #%u (%u/%u records)\r\n(G)roup ID\r\n(N)ew device ID\n\r(F)ind neighbors\r\n(C)reate record on neighbor\r\n(D)elete record on neighbor\r\n(R)etrieve record from neighbor\r\n(S)how local records\r\nR(e)set local storage\r\n\r\nSelection: ", groupID, nodeID, entryCount, DATABASE_SIZE);
+
+	// Get user input
+	state MENU_INPUT :
+		ser_inf(MENU_INPUT, "%c", &c);
+
+	switch (c)
+	{
+		case 'g': case 'G': proceed MENU; 		// todo
+		case 'n': case 'N': proceed MENU; 		// todo
+		case 'f': case 'F': proceed MENU; 		// todo
+		case 'c': case 'C': proceed MENU; 		// todo
+		case 'd': case 'D': proceed MENU; 		// todo
+		case 'r': case 'R': proceed MENU; 		// todo
+		case 's': case 'S': proceed MENU; 		// todo
+		case 'e': case 'E': proceed MENU; 		// todo
+		default: 			proceed MENU;
+	}
+}
--- app.h
+++ app.h
@@ -5,11 +5,10 @@
  *			Desmond Stular (3067040)
  *
  * 	  Date: March 19, 2024
-*/
+ */
 
 #ifndef APP_H_
 #define APP_H_
-
 
 #include "phys_cc1350.h"
 #include "plug_null.h"
@@ -18,12 +17,10 @@
 #include "sysio.h"
 #include "tcv.h"
 
-
 // Maximum sizes
 #define RECORD_SIZE 20
 #define DATABASE_SIZE 40
 #define PACKET_LEN 250
-
 
 // Message Types
 #define DIS_REQ 0x00
@@ -33,13 +30,11 @@
 #define RET_REC 0x04
 #define RES_MSG 0x05
 
-
 // Response message statuses
 #define SUCCESS 0x01
 #define DATA_FULL 0x02
 #define DELETE_FAIL 0x03
 #define NO_ENTRY 0x04
-
 
 /*** Message structures ***/
 
@@ -48,84 +43,89 @@
  * and obtain a list of reachable neighbors that
  * belongs to the node group. Also used by node
  * to identify the structure of a receiving message.
-*/
-struct discoveryMsg {
-  word groupID;
-  byte type;
-  byte requestNum;
-  byte senderID;
-  byte receiverID;
+ */
+struct discoveryMsg
+{
+	word groupID;
+	byte type;
+	byte requestNum;
+	byte senderID;
+	byte receiverID;
 };
 
 /*
  * Message is sent when a sender node wishes to
  * create and store a record in another node node.
-*/
-struct newRecordMsg {
-  word groupID;
-  byte type;
-  byte requestNumber;
-  byte senderID;
-  byte receiverID;
-  char record[RECORD_SIZE];
+ */
+struct newRecordMsg
+{
+	word groupID;
+	byte type;
+	byte requestNumber;
+	byte senderID;
+	byte receiverID;
+	char record[RECORD_SIZE];
 };
 
 /*
  * Message is sent when a sender node wishes to
  * delete a record stored in another node.
-*/
-struct delRecordMsg {
-  word groupID;
-  byte type;
-  byte requestNumber;
-  byte senderID;
-  byte receiverID;
-  char record[RECORD_SIZE];
-  byte recordIndex;
-  byte padding; //not used
+ */
+struct delRecordMsg
+{
+	word groupID;
+	byte type;
+	byte requestNumber;
+	byte senderID;
+	byte receiverID;
+	char record[RECORD_SIZE];
+	byte recordIndex;
+	byte padding; // not used
 };
 
 /*
  * Message is sent when a sender node wishes to
  * retrieve a record stored in another node.
-*/
-struct retRecordMsg {
-  word groupID;
-  byte type;
-  byte requestNumber;
-  byte senderID;
-  byte receiverID;
-  char record[RECORD_SIZE];
-  byte recordIndex;
-  byte padding; //not used
+ */
+struct retRecordMsg
+{
+	word groupID;
+	byte type;
+	byte requestNumber;
+	byte senderID;
+	byte receiverID;
+	char record[RECORD_SIZE];
+	byte recordIndex;
+	byte padding; // not used
 };
 
 /*
  * This message is formed and sent by a node
  * after receiving a request message (Create
- * Record Message, Delete, or Retrieve) and 
+ * Record Message, Delete, or Retrieve) and
  * after performing the corresponding action.
-*/
-struct responseMsg {
-  word groupID;
-  byte type;
-  byte requestNumber;
-  byte senderID;
-  byte receiverID;
-  byte status;
-  byte padding;
-  char record[RECORD_SIZE];
+ */
+struct responseMsg
+{
+	word groupID;
+	byte type;
+	byte requestNumber;
+	byte senderID;
+	byte receiverID;
+	byte status;
+	byte padding;
+	char record[RECORD_SIZE];
 };
 
 /*
  * The Database struct
  * contains ownerID timeStamp and string message
  */
-struct DB{
+struct dbEntry
+{
 	word ownerID;
 	lword timeStamp;
 	char record[RECORD_SIZE];
 };
 
 #endif
-
